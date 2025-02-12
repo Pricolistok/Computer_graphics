76b279025d4cb0f2852b603ba983eaaae077a944
Pricolistok <dokolin05@mail.ru>
2025-02-12 10:34:09 +0300

    add work with table

--- lab_01/mainwindow.cpp
+++ lab_01/mainwindow.cpp
@@ -2,19 +2,24 @@
 #include "ui_mainwindow.h"
 #include <QPushButton>
 #include <QMessageBox>
+#include <iostream>
+#include <QPainter>
+
 using namespace std;
 
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(print_text()));
+    connect(ui->pushBtnAddDot, SIGNAL(clicked()), this, SLOT(print_text()));
+    cout << "hhh" <<endl;
 }
 
 void MainWindow::print_text()
 {
-    QMessageBox msgBox;
-    msgBox.setText("Hello");
-    msgBox.exec();
+    ui->tableWidget->insertRow(ui->tableWidget->rowCount());
+    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, 0, new QTableWidgetItem(ui->lineEdit_X->text()));
+    ui->tableWidget->setItem(ui->tableWidget->rowCount()-1, 1, new QTableWidgetItem(ui->lineEdit_Y->text()));
+    cout << ui->lineEdit_X->text().toStdString() << endl;
 }
 
 MainWindow::~MainWindow()
